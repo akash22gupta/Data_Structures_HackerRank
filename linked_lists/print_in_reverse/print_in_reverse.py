165c1d196d84ffeb8ed61fa87486c87905b6b58b
Akash Gupta <akash@hawk.iit.edu>
2018-06-29 23:36:40 -0500

    print in reverse

---
+++ linked_lists/print_in_reverse/print_in_reverse.py
@@ -0,0 +1,16 @@
+# Complete the reversePrint function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def reversePrint(head):
+    if head==None:
+        return
+    reversePrint(head.next)
+    print(head.data)
+    return
